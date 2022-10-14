6450c730b4e971d78ac28d9bb12d9b25cb1dfba6
Eduardo Marques <marquesaedu@gmail.com>
2022-10-14 15:38:52 -0300

    Código inicial de DFS

---
+++ DFS/dfsLista.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int numVertices, numArestas;
+
+    cin >> numVertices >> numArestas;
+
+    vector<vector<int>> listaAdjacencia(numVertices, vector<int>(0));
+
+    int u, v;
+    for(int i = 0; i < numArestas; i++)
+    {
+        cin >> u >> v;
+        listaAdjacencia[u].push_back(v);
+        listaAdjacencia[v].push_back(u);
+    }
+
+
+    cout << "Hello World!" << endl; 
+
+    return 0;
+}
